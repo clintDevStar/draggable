c0581ed420ada3500bdb8493513035bb0574c800
ClintPy <cmaruti93@gmail.com>
2020-01-09 14:16:50 +0300

    [finishes #001]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          taskID: 1,
+          task: "Walk the walk"
+        },
+        {
+          taskID: 2,
+          task: "Talk the talk"
+        },
+        {
+          taskID: 3,
+          task: "Jump the jump"
+        }
+      ],
+      completedTasks: [],
+      draggedTask: {}
+    };
+  }
+
+  onDrag = (event, todo) => {
+    event.preventDefault();
+    this.setState({
+      draggedTask: todo
+    });
+  };
+
+  onDragOver = event => {
+    event.preventDefault();
+  };
+
+  onDrop = event => {
+    const { completedTasks, draggedTask, todos } = this.state;
+    this.setState({
+      completedTasks: [...completedTasks, draggedTask],
+      todos: todos.filter(task => task.taskID !== draggedTask.taskID),
+      draggedTask: {}
+    });
+  };
+  render() {
+    const { todos, completedTasks } = this.state;
+    return (
+      <div className="App">
+        <div className="todos">
+          {todos.map(todo => (
+            <div
+              key={todo.taskID}
+              draggable
+              onDrag={event => this.onDrag(event, todo)}
+              className = "item"
+            >
+              {todo.task}
+            </div>
+          ))}
+        </div>
+        <div
+          onDrop={event => this.onDrop(event)}
+          onDragOver={event => this.onDragOver(event)}
+          className="done"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {completedTasks.map((task, index) => (
+            <div key={task.taskID} className="item">{task.task}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
